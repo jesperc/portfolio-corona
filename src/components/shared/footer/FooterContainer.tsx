c9c6ef012236e6cc0b7fe097a88f57d21d90f115
Jesper Corell <jesper.corell@gmail.com>
2020-04-26 15:03:11 +0200

    :sparkles: add linkedin button in footer

--- src/components/shared/footer/FooterContainer.tsx
+++ src/components/shared/footer/FooterContainer.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './footer.scss'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
 const FooterContainer: React.FC = () => {
   const copyrightSymbol = '\u00a9'
   const fullYear = new Date().getFullYear()
+  const linkedInUrl = 'https://www.linkedin.com/in/jesper-corell-3656372b/'
 
   return (
     <div className="centered">
-      <div className="footer">{`Made by Jesper Corell ${copyrightSymbol} ${fullYear}`}</div>
+      <div className="footer">
+        <div className="text">
+          {`Made by Jesper Corell ${copyrightSymbol} ${fullYear}`}
+          <a href={linkedInUrl}>
+            <LinkedInIcon className="icon footer-icon" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
