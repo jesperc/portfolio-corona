2c4174f888210e8db12bf3ad561ff9678814983f
Jesper Corell <jesper.corell@gmail.com>
2020-05-03 00:39:38 +0200

    :sparkles: use hash router

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from '../shared/header'
 import Project from './project/project'
 import Footer from '../shared/footer'
